cbd906224b7c64c074bf059004badfa34be36e44
Diego González <the_sorr0w@hotmail.com>
2021-12-12 19:13:15 -0300

    commit

---
+++ src/includes/header.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="src/stylesheet/style.css">
+<div class='navContainer'>
+    <div class='navIcon'>
+        <h1>Bsale</h1>
+    </div>
+    <div class='ddCategorias'>
+        <button>Categorías</button>
+    </div>
+    <div class='navBusquedaContainer'>
+        <input type='text' id='txtBuscar' class='navTextoBusqueda' placeholder="Buscar Productos" />
+        <input type='button' id='btnBuscar' class='navBtnBusqueda' value="Buscar" />
+    </div>
+    <div class='navCarroContainer'>
+        <p>Carro</p>
+    </div>
+</div>
