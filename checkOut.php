35ef799fca72877df4ec66d552c4d941efe5f506
Diego González <the_sorr0w@hotmail.com>
2021-12-12 16:09:19 -0300

    revisión

--- checkOut.php
+++ checkOut.php
@@ -1,56 +1,55 @@
 <!DOCTYPE html>
 <html>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <head>
-    <!--Imports-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="src/js/carro.js"></script>
-    <link rel="stylesheet" href="src/stylesheet/style.css" />
-  </head>
+<head>
+  <!--Imports-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="src/js/app.js"></script>
+  <link rel="stylesheet" href="src/stylesheet/style.css" />
+</head>
 
-  <body>
-    <section>
-      <div class="carroWrapper">
-        <!--Listado de productos-->
-        <div class="contenedorListaProductos">
-          <div class="carroTitulo">
-            <div><a href="index.html">Volver</a></div>
-            <h1>Carro | 1 Productos</h1>
-            <hr />
-          </div>
-          <!--Items-->
-          <div
-            id="carroList"
-            style="height: 600px; padding: 10px; overflow: auto"
-          ></div>
-          <!--Fin de items-->
+<body>
+  <section>
+    <div class="carroWrapper">
+      <!--Listado de productos-->
+      <div class="contenedorListaProductos">
+        <div class="carroTitulo">
+          <div><a href="index.php">Volver</a></div>
+          <h1>Carro | 1 Productos</h1>
+          <hr />
         </div>
-        <!--Fin de listado de productos-->
-        <!--Total-->
-        <div class="contenedorPagos">
-          <div class="carroTitulo">
-            <h1>Resumen de la compra</h1>
-            <hr />
-          </div>
-          <div class="contenedorItems">
-            <div class="carroItem-int">
-              <h3>Total a pagar:</h3>
-              <span id="totalPagar"></span>
-            </div>
-            <div class="carroItem-int">
-              <input type="button" value="Pagar" />
-            </div>
-          </div>
-        </div>
-        <!--Fin de total-->
+        <!--Items-->
+        <div id="carroList" style="height: 600px; padding: 10px; overflow: auto"></div>
+        <!--Fin de items-->
       </div>
-    </section>
-  </body>
-  <script>
-    window.onload = function () {
-      cargarCarro();
-      listarArticulos();
-    };
-  </script>
+      <!--Fin de listado de productos-->
+      <!--Total-->
+      <div class="contenedorPagos">
+        <div class="carroTitulo">
+          <h1>Resumen de la compra</h1>
+          <hr />
+        </div>
+        <div class="contenedorItems">
+          <div class="carroItem-int">
+            <h3>Total a pagar:</h3>
+            <span id="totalPagar"></span>
+          </div>
+          <div class="carroItem-int">
+            <input type="button" value="Pagar" />
+          </div>
+        </div>
+      </div>
+      <!--Fin de total-->
+    </div>
+  </section>
+</body>
+<script>
+  //Inicializar items
+  window.onload = function() {
+    cargarCarro();
+    listarArticulos();
+  };
+</script>
+
 </html>
