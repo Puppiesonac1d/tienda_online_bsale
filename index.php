dc14c73d66d5f729a5d24e70138655d4a162da72
Diego González <the_sorr0w@hotmail.com>
2021-12-12 16:11:15 -0300

    Eliminar notify

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="src/stylesheet/style.css" />
   <script src="src/js/app.js"></script>
-  <script src="src/js/notify.js"></script>
   <title>Document</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
