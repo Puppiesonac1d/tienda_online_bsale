0ce1dc9c2932b25df0f715cb308655baecb79e1e
Diego González <the_sorr0w@hotmail.com>
2021-12-12 22:06:34 -0300

    muchos fixes

--- src/api/busquedaAPI.php
+++ src/api/busquedaAPI.php
@@ -1,5 +1,19 @@
 <?php
-include 'conn.php';
+//Establecer la conexión a la base de datos.
+//Parámetros:
+$hostURL = 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com';
+$dbUsuario = 'bsale_test';
+$dbPass = 'bsale_test';
+$dbEsquema = 'bsale_test';
+
+//Abrir conexión
+$conn = mysqli_connect($hostURL, $dbUsuario, $dbPass, $dbEsquema);
+
+mysqli_set_charset($conn, "utf8");
+
+if ($conn->connect_error) {
+    die('Conexión fallida con el host: ' . $conn->connect_error);
+}
 header('Content-Type: JSON');
 //Si se recibe un GET con parámetros de búsqueda...
 //Cargar productos
