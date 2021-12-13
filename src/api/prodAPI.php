23d546ea4dc3bf66716f3867533903e0b4ac3684
Diego González <the_sorr0w@hotmail.com>
2021-12-12 22:06:05 -0300

    muchos fixes

--- src/api/prodAPI.php
+++ src/api/prodAPI.php
@@ -1,11 +1,31 @@
 <?php
-include 'conn.php';
 header('Content-Type: JSON');
+
+
+//Establecer la conexión a la base de datos.
+//Parámetros:
+$hostURL = 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com';
+$dbUsuario = 'bsale_test';
+$dbPass = 'bsale_test';
+$dbEsquema = 'bsale_test';
+
+//Abrir conexión
+$conn = mysqli_connect($hostURL, $dbUsuario, $dbPass, $dbEsquema);
+
+mysqli_set_charset($conn, "utf8");
+
+if ($conn->connect_error) {
+    die('Conexión fallida con el host: ' . $conn->connect_error);
+}
 
 
 //Si se recibe un GET con parámetros de búsqueda...
 //Cargar productos
-$query = "select p.id as idProd,p.name as prod,p.url_image,p.price,p.discount, c.name as cat from product p join category c on p.category = c.id;";
+
+$nomProd = $_GET['nomProd'];
+
+$query = "select p.id as idProd,p.name as prod,p.url_image,p.price,p.discount, c.name as cat from product p join category c on p.category = c.id 
+where  LOWER(p.name) like LOWER('%$nomProd%');";
 $result = mysqli_query($conn, $query);
 $arr = array();
 
